afbb5762eb3379b6236ada73db1094f22bcfa2cd
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2019-10-20 05:40:12 +0700

    Perbaikan

--- Source.cpp
+++ Source.cpp
@@ -9,7 +9,7 @@ using namespace std;
 class Balok																					//Nama Class
 {
 public:																						//Access Modifier
-	string Pertanyaan[4], Menu[1], Peringatan;												//Atribut
+	string Pertanyaan[4], Menu[2], Hasil[3], Peringatan;									//Atribut
 	string Panah = "=> ";
 	char GarisMin = '-';																	//Atribut (Assignment)
 
@@ -54,58 +54,73 @@ public:																						//Access Modifier
 
 int main()
 {
-	int PilihanMenu;
-	int P, L, T;										
+	int PilihanMenu, JumlahMenu = 3, X = 0;
+	int P, L, T;
 	double V;
-	Balok Input, Output, Pesan, Hasil;
+	Balok Input, Output, Pesan, Hasil, TampilSub;
 
 	Input.Pertanyaan[0] = "Masukkan Nilai Panjang  : ";
 	Input.Pertanyaan[1] = "Masukkan Nilai Lebar    : ";
 	Input.Pertanyaan[2] = "Masukkan Nilai Tinggi   : ";
 	Input.Pertanyaan[3] = "Masukkan Nilai Volume   : ";
 	Input.Pertanyaan[4] = "Pilih : ";
-	Output.Menu[0] = "# Hitung: Volume & Luas Permukaan";
-	Output.Menu[1] = "# Hitung: Panjang & Lebar";
+	Output.Menu[0] = "Hitung: Volume & Luas Permukaan";
+	Output.Menu[1] = "Hitung: Panjang";
+	Output.Menu[2] = "Hitung: Lebar";
 	Pesan.Peringatan = "Terjadi Kesalahan Tidak Terduga";
-
-	//Input.Judul[0] = "INPUT";
-	//Input.Judul[1] = "OUTPUT";
-	//Input.CetakGaris(Input.Judul[0]);
+	TampilSub.Hasil[0] = "Volume          = ";
+	TampilSub.Hasil[1] = "Luas Permukaan  = ";
+	TampilSub.Hasil[2] = "Panjang  = ";
+	TampilSub.Hasil[3] = "Lebar    = ";
 
 	Input.CetakJudul("PILIH MENU"); Input.CetakGaris();
-	cout << Output.Menu[0] << endl << Output.Menu[1] << endl; Input.CetakGaris();
-	cout << Input.Pertanyaan[4]; cin >> PilihanMenu; cout << endl;
 
+	for (int i = 1; i <= JumlahMenu; i++)
+	{
+		cout << "[" << i << "] " << Output.Menu[X] << endl;
+		X++;
+	}
+
+	Input.CetakGaris(); 
+	cout << Input.Pertanyaan[4]; cin >> PilihanMenu; cout << endl;
+	
 	Input.CetakJudul("INPUT"); Input.CetakGaris(); 
 
-	switch (PilihanMenu)
+	try
 	{
-	case 1:
-		cout << Input.Pertanyaan[0]; cin >> P;
-		cout << Input.Pertanyaan[1]; cin >> L;
-		cout << Input.Pertanyaan[2]; cin >> T;
-		Input.CetakJudul("OUTPUT"); Input.CetakGaris();
-		cout << Output.Panah << Hasil.Hasil_Volume(P, L, T) << endl;
-		cout << Output.Panah << Hasil.Hasil_LuasPermukaan(P, L, T) << endl;
-		break;
-	case 2:
-		try
+		switch (PilihanMenu)
 		{
+		case 1:
+			cout << Input.Pertanyaan[0]; cin >> P;
+			cout << Input.Pertanyaan[1]; cin >> L;
+			cout << Input.Pertanyaan[2]; cin >> T;
+			Input.CetakJudul("OUTPUT"); Input.CetakGaris();
+			cout << Output.Panah << TampilSub.Hasil[0] << Hasil.Hasil_Volume(P, L, T) << endl;
+			cout << Output.Panah << TampilSub.Hasil[1] << Hasil.Hasil_LuasPermukaan(P, L, T) << endl;
+			break;
+		case 2:
+
 			cout << Input.Pertanyaan[3]; cin >> V;
 			cout << Input.Pertanyaan[1]; cin >> L;
 			cout << Input.Pertanyaan[2]; cin >> T;
 			Input.CetakJudul("OUTPUT"); Input.CetakGaris();
-			cout << Output.Panah << Hasil.Hasil_Panjang(V, (double)L, (double)T) << endl;
-			cout << Output.Panah << Hasil.Hasil_Lebar(V, (double)P, (double)T) << endl;
-		}
-		catch (const std::exception&)
-		{
+			cout << Output.Panah << TampilSub.Hasil[2] << Hasil.Hasil_Panjang(V, (double)L, (double)T) << endl;
+			break;
+		case 3:
+			cout << Input.Pertanyaan[3]; cin >> V;
+			cout << Input.Pertanyaan[0]; cin >> P;
+			cout << Input.Pertanyaan[2]; cin >> T;
+			Input.CetakJudul("OUTPUT"); Input.CetakGaris();
+			cout << Output.Panah << TampilSub.Hasil[3] << Hasil.Hasil_Lebar(V, (double)P, (double)T) << endl;
+			break;
+		default:
 			cout << endl << Output.Peringatan;
+			break;
 		}
-		break;
-	default:
+	}
+	catch (const std::exception&)
+	{
 		cout << endl << Output.Peringatan;
-		break;
 	}
 
 	_getch();
